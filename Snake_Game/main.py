95441caf235eaf25793e058b2984b1e41eabbbf0
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-04 19:20:47 +0530

    Update snake game

---
+++ Snake_Game/main.py
@@ -0,0 +1,8 @@
+print("Snake Game")
+from turtle import Turtle,Screen
+
+screen=Screen()
+screen.title("Naren's Snake Game Python")
+screen.setup(1000,600)
+screen.bgcolor("black")
+screen.exitonclick()
