613980520670c8af6e983234c8f80f1243f09e2d
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-04 21:47:55 +0530

    Update snake game

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -1,20 +1,22 @@
+import time
+
 print("Snake Game")
 from turtle import Turtle,Screen
-
-screen=Screen()
+from snake import Snake
+screen = Screen()
+screen.tracer(0)
 screen.colormode(255)
 screen.title("Naren's Snake Game Python")
 screen.setup(700,700)
 screen.bgcolor("black")
 
-positions=[(0,0), (-20,0), (-40,0)]
-snake_body=[]
-for position in positions:
-    new_div = Turtle("square")
-    new_div.penup()
-    new_div.goto(position)
-    new_div.pendown()
-    new_div.color((204,255,0))
-    snake_body.append(new_div)
+snake= Snake()
+
+game_status = True
+while game_status:
+    screen.update()
+    time.sleep(0.2)
+    snake.move()
+
 
 screen.exitonclick()
---
+++ Snake_Game/snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+positions=[(0,0), (-20,0), (-40,0)]
+
+
+class Snake:
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in positions:
+            new_div = Turtle("square")
+            new_div.penup()
+            new_div.goto(position)
+            # new_div.pendown()
+            new_div.color((204, 255, 0))
+            self.snake_body.append(new_div)
+
+    def move(self):
+        for part in range(len(self.snake_body) - 1, 0, -1):
+            prev_x = self.snake_body[part - 1].xcor()
+            prev_y = self.snake_body[part - 1].ycor()
+            # snake_body[part].penup()
+            self.snake_body[part].goto(prev_x, prev_y)
+
+        self.snake_body[0].forward(20)
+        self.snake_body[0].right(90)
