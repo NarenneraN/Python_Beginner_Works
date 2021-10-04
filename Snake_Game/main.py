c9450dcb7965ea17878bce8fed4115df4768267c
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-04 20:35:28 +0530

    Update snake game

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -2,7 +2,19 @@ print("Snake Game")
 from turtle import Turtle,Screen
 
 screen=Screen()
+screen.colormode(255)
 screen.title("Naren's Snake Game Python")
-screen.setup(1000,600)
+screen.setup(700,700)
 screen.bgcolor("black")
+
+positions=[(0,0), (-20,0), (-40,0)]
+snake_body=[]
+for position in positions:
+    new_div = Turtle("square")
+    new_div.penup()
+    new_div.goto(position)
+    new_div.pendown()
+    new_div.color((204,255,0))
+    snake_body.append(new_div)
+
 screen.exitonclick()
