8a8cfae4fb3eabed7d5b19617b3c434a633f8d4c
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-05 12:15:01 +0530

    Update snake game

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -1,29 +1,31 @@
 import time
-
-print("Snake Game")
-from turtle import Turtle,Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 screen = Screen()
+
+# while True:
+#  diff_level=turtle.numinput("Difficulty of Level","1.Easy\n2.Medium\n3.Difficult")
+#  if (diff_level==1 or diff_level==2 or diff_level==3):
+#      break
+#  turtle.write("Wrong Input!")
+
 screen.tracer(0)
 screen.colormode(255)
 screen.title("Naren's Snake Game Python")
 screen.setup(700,700)
 screen.bgcolor("black")
 
-snake= Snake()
+snake = Snake()
+food = Food()
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 game_status = True
 while game_status:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.1)
     snake.move()
-
-
-
-
-
 screen.exitonclick()
