92133ad21662d00cfafb94b9a67c593193391d44
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-05 22:57:19 +0530

    update snake game

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -3,19 +3,15 @@ from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Score
+from level import Level
 screen = Screen()
-
-# while True:
-#  diff_level=turtle.numinput("Difficulty of Level","1.Easy\n2.Medium\n3.Difficult")
-#  if (diff_level==1 or diff_level==2 or diff_level==3):
-#      break
-#  turtle.write("Wrong Input!")
-
-screen.tracer(0)
 screen.colormode(255)
+screen.tracer(0)
 screen.title("Naren's Snake Game Python")
 screen.setup(700,700)
 screen.bgcolor("black")
+
+level_diff = Level()
 
 snake = Snake()
 food = Food()
@@ -26,9 +22,10 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 game_status = True
+
 while game_status:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(level_diff.speed)
     snake.move()
     if snake.snake_body[0].distance(food) < 15:
         print("Caught that!")
