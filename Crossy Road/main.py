ac20e4d627166f20e69d97f817dddf922399000d
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-06 18:23:05 +0530

    Update Snake game

---
+++ Crossy Road/main.py
@@ -0,0 +1,10 @@
+print("Naren's Crossy Road 😂")
+from turtle import Turtle,Screen
+from boundary import Boundary
+screen=Screen()
+screen.bgcolor("black")
+screen.setup(900,700)
+screen.tracer(0)
+boundary=Boundary()
+screen.update()
+screen.exitonclick()
