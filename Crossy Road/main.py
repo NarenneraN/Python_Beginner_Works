2b5e70d55add65e676518c3f3be728d7544d7492
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-06 22:01:54 +0530

    Update Crossy Road

--- Crossy Road/main.py
+++ Crossy Road/main.py
@@ -1,10 +1,18 @@
 print("Naren's Crossy Road 😂")
 from turtle import Turtle,Screen
 from boundary import Boundary
+from vehicle import Vehicle
+status=True
 screen=Screen()
 screen.bgcolor("black")
 screen.setup(900,700)
 screen.tracer(0)
 boundary=Boundary()
-screen.update()
+vehicle=Vehicle()
+screen.listen()
+screen.onkey(vehicle.up, "Up")
+screen.onkey(vehicle.up, "w")
+while status:
+    screen.update()
+
 screen.exitonclick()
