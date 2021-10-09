1988d8140879b6ef6e384943307b6be86c37f126
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-09 19:20:48 +0530

    High Score added

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -5,6 +5,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("score.txt") as score_file:
+         self.high_score=int(score_file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -21,11 +23,19 @@ class Score(Turtle):
 
     def game_over(self):
         self.clear()
-        self.goto(0,0)
+        self.goto(0,60)
         self.write(f"Game Over", font=('Courier', 80, 'italic'), align='center')
-        self.goto(0,-70)
+        self.goto(0,-0)
         self.write(f"Score :- {self.score}", font=('Courier', 24, 'italic'), align='center')
 
+        if self.score>self.high_score:
+            self.goto(0,-160)
+            self.write(f"Woah! It's High Score 🔥😎", font=('Courier', 24, 'italic'), align='center')
+            with open("score.txt","w") as score_file:
+                self.high_score = self.score
+                score_file.write(str(self.high_score))
+        self.goto(0,-60)
+        self.write(f"\n\nHigh Score :- {self.high_score}", font=('Courier', 30, 'italic'), align='center')
 
 
 
