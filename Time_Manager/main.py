8f0b6528611d04c7df2f9257be235081a42a6ba4
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-14 04:51:16 +0530

    Canvas operations

--- Time_Manager/main.py
+++ Time_Manager/main.py
@@ -1,7 +1,15 @@
 from tkinter import *
+from PIL import Image, ImageTk
 print("Simple time manger!")
 window =Tk()
 window.title("Time Manager")
 window.config(padx=200,pady=120)
+
+canvas = Canvas(width=300,height=300)
+image = Image.open("timer.png")
+timer_image_r= image.resize((300,300))
+timer_image = ImageTk.PhotoImage(timer_image_r)
+canvas.create_image(150,150,image=timer_image)
+canvas.pack()
 # Footer
 window.mainloop()
