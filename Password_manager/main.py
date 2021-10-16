62c708a8f97e18acb733f2f31e466b689c66db15
NarenneraN <narendhiran.pugazhendhi@gmail.com>
2021-10-16 22:54:23 +0530

    Update password manger

--- Password_manager/main.py
+++ Password_manager/main.py
@@ -1,5 +1,42 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+label_website = Label(text="Website:")
+label_website.grid(row=1, column=0)
+label_email_username = Label(text="Email/Username:")
+label_email_username.grid(row=2, column=0)
+label_password = Label(text="Password:")
+label_password.grid(row=3, column=0)
+
+website_entry = Entry()
+website_entry.config(width=52)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+username_entry = Entry()
+username_entry.config(width=52)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry()
+password_entry.config(width=33)
+password_entry.grid(column=1, row=3)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add")
+add_button.config(width=44)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
